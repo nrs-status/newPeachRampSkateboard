af1c5d0a72e9ee93d5ad94e91dc7c3bcadd37a7f
nrs-status <sebaarsim@gmail.com>
2026-08-24 04:00:07 +0000

    made an arg default

--- billProjectsBelt/listPathsSatisfyingPred.nix
+++ billProjectsBelt/listPathsSatisfyingPred.nix
@@ -1,5 +1,5 @@
 { pkgsLib }:
-{ dir, pred, excludeDirectories ? false }:
+{ dir ? ./., pred, excludeDirectories ? false }:
 (import ./withDebug.nix) rec {
   fileListing = pkgsLib.filesystem.listFilesRecursive dir;
   totalListing = if excludeDirectories then fileListing else fileListing ++ pkgsLib.lists.unique (map dirOf fileListing);
